231c7ac375d23803aba1d0e87c0817ba4ad784f7
Luise Brandenburger <luise.brandenburger@ts.berlin>
2024-08-28 12:58:29 +0200

    feat: update and cleanup abimo handler

--- addons/abimoHandler/components/AbimoCalcButton.vue
+++ addons/abimoHandler/components/AbimoCalcButton.vue
@@ -1,5 +1,5 @@
 <script>
-import styleList from "@masterportal/masterportalapi/src/vectorStyle/styleList";
+// import styleList from "@masterportal/masterportalapi/src/vectorStyle/styleList";
 import { mapGetters } from "vuex";
 import Style from "ol/style/Style";
 import Fill from "ol/style/Fill";
@@ -34,29 +34,75 @@ export default {
       .getLayers()
       .getArray()
       .find((layer) => layer.get("id") === "planung_abimo");
+
+    this.layer_abimo_result_infiltration = mapCollection
+      .getMap("2D")
+      .getLayers()
+      .getArray()
+      .find((layer) => layer.get("id") === "abimo_result_infiltration");
+
+    this.layer_abimo_result_evaporation = mapCollection
+      .getMap("2D")
+      .getLayers()
+      .getArray()
+      .find((layer) => layer.get("id") === "abimo_result_evaporation");
+
+    this.layer_abimo_result_surface_run_off = mapCollection
+      .getMap("2D")
+      .getLayers()
+      .getArray()
+      .find((layer) => layer.get("id") === "abimo_result_surface_run_off");
   },
   methods: {
-    /**
-     * Maps the selected features to new features with updated values based on the response.
-     * @param {Array} response - The response object containing the updated values for each feature.
-     * @return {Array} An array of new features with updated values.
-     */
-    async mapToolFeatures(features, response) {
+    async processAndAddFeatures(features, response) {
+      const layers = [
+        this.layer_abimo_result_infiltration,
+        this.layer_abimo_result_evaporation,
+        this.layer_abimo_result_surface_run_off,
+      ];
+
+      // Clear all layers
+      layers.forEach((layer) => layer.getSource().clear());
       this.layer_abimo_calculated.values_.source.clear();
 
-      return features.map((featureData) => {
-        const values = response.find(
-          (item) => item.code === featureData.getProperties().code,
-        );
-        return new Feature({
-          ...featureData.values_,
+      const createFeatureForLayer = (featureData, values, propertyName) => {
+        // console.log("[AbimoCalcButton] values::", values);
+
+        const newFeature = new Feature({
           geometry: featureData.getGeometry(),
-          new_evaporation: values?.evaporation || 200,
-          new_infiltration: values?.infiltration || 500,
-          new_surface_runoff: values?.surface_runoff || 10,
+          [propertyName]: values[propertyName] || 500,
         });
+
+        const style = this.createStyleForProperty(
+          propertyName,
+          values[propertyName] || 0,
+        );
+        newFeature.setStyle(style);
+
+        return newFeature;
+      };
+
+      const layerData = [
+        { property: "new_infiltration", style: "abimo_new_infiltration" },
+        { property: "new_evaporation", style: "abimo_new_evaporation" },
+        { property: "new_surface_runoff", style: "abimo_new_surface_runoff" },
+      ];
+
+      layerData.forEach((data, index) => {
+        const newFeatures = features.map((featureData) => {
+          const values = response.find(
+            (item) => item.code === featureData.getProperties().code,
+          );
+          return createFeatureForLayer(featureData, values, data.property);
+        });
+
+        // Add features directly to the layer
+        layers[index].getSource().addFeatures(newFeatures);
       });
+
+      this.isCalculated = true;
     },
+
     /**
      * Fetches multiblock data, processes it, and updates the map features accordingly.
      * @return {Promise<void>} A promise that resolves when the data has been fetched and processed.
@@ -64,7 +110,6 @@ export default {
     async fetchCalculateMultiblock() {
       const mapFeatures =
         this.layer_abimo_calculated.values_.source.getFeatures();
-      console.log("[AbimoCalcButton]  mapFeatures::", mapFeatures);
 
       // get multiblock data
       let payload = {
@@ -84,49 +129,44 @@ export default {
 
       try {
         // console.log("[AbimoHandler] payload::", payload);
+
+        // const payload = {
+        //   features: [{ feature: 1 }, { feature: 2 }, { feature: 3 }],
+        //   targets: {
+        //     new_green_roof: 0.35,
+        //     new_to_swale: 0.2,
+        //     new_unpvd: 0.17,
+        //   },
+        // };
+
         // const data = await getRabimo.getMultiblock(payload);
         // console.log("[AbimoMeasure] data::", data);
 
         const testResponse = [
           {
-            code: "1100541271000000",
+            code: "1100551151000000",
             area: 4951.854,
-            surface_runoff: 291.224,
-            infiltration: 153.208,
-            evaporation: 187.569,
+            new_surface_runoff: 291.224,
+            new_infiltration: 153.208,
+            new_evaporation: 187.569,
           },
           {
             code: "1100551171000000",
             area: 3406.417,
-            surface_runoff: 299.907,
-            infiltration: 146.991,
-            evaporation: 184.102,
+            new_surface_runoff: 299.907,
+            new_infiltration: 146.991,
+            new_evaporation: 184.102,
           },
           {
             code: "1100541281000000",
             area: 4985.961,
-            surface_runoff: 403.332,
-            infiltration: 78.991,
-            evaporation: 145.677,
+            new_surface_runoff: 403.332,
+            new_infiltration: 78.991,
+            new_evaporation: 145.677,
           },
         ];
 
-        // map response to features
-        const olFeatures = await this.mapToolFeatures(
-          mapFeatures,
-          testResponse,
-        );
-
-        // Aufgabe: 3 Layer mit den jeweiligen Werten anlegen oder einen Layer filtern?
-        for (const feature of olFeatures) {
-          const properties = feature.getProperties();
-          this.createStyle(properties)[0];
-
-          // feature.setStyle("abimo");
-          feature.setStyle(this.createStyle(properties)[1]);
-        }
-
-        this.addToLayer(olFeatures);
+        await this.processAndAddFeatures(mapFeatures, testResponse);
         this.isCalculated = true;
       } catch (error) {
         console.error("Fehler beim Abrufen der Daten:", error);
@@ -135,81 +175,30 @@ export default {
       }
     },
 
-    getColorWithOpacity(value, min = 0, max = 672) {
+    getOpacity(value, min = 0, max = 672) {
       const normalized = (value - min) / (max - min);
-      const opacity = Math.max(0, Math.min(1, normalized));
+      return Math.max(0, Math.min(1, normalized));
+    },
 
-      return {
-        runoff: `rgba(255, 0, 0, ${opacity})`,
-        infiltration: `rgba(0, 255, 0, ${opacity})`,
-        evaporation: `rgba(0, 0, 255, ${opacity})`,
+    createStyleForProperty(propertyName, value) {
+      const colorMap = {
+        new_infiltration: "rgba(0, 255, 0, ${opacity})",
+        new_evaporation: "rgba(0, 0, 255, ${opacity})",
+        new_surface_runoff: "rgba(255, 0, 0, ${opacity})",
       };
-    },
 
-    /**
-     * Transforms imported geodata into a style object based on predefined color rules.
-     * @param {Object} properties - An object containing geodata properties.
-     * @return {Style} A style object with stroke and fill properties.
-     */
-    createStyle(properties) {
-      // This function transform importet geodata
-      const styleObject = styleList.returnStyleObject("abimo");
-      console.log("[AbimoCalcButton] styleObject::", styleObject);
+      const opacity = this.getOpacity(value || 0, 0, 672);
+      const color = colorMap[propertyName].replace("${opacity}", opacity);
 
-      const surfaceRunoff = properties.new_surface_runoff;
-      const infiltration = properties.new_infiltration;
-      const evaporation = properties.new_evaporation;
-
-      // Berechnete Farben für jeden Wert
-      const runoffColor = this.getColorWithOpacity(surfaceRunoff).runoff;
-      const infiltrationColor =
-        this.getColorWithOpacity(infiltration).infiltration;
-      const evaporationColor =
-        this.getColorWithOpacity(evaporation).evaporation;
-
-      return [
-        new Style({
-          stroke: new Stroke({
-            color: `rgba(255, 0, 0, 1)`,
-            width: 2,
-          }),
-          fill: new Fill({
-            color: runoffColor,
-          }),
+      return new Style({
+        stroke: new Stroke({
+          color: "rgba(0, 0, 0, 1)",
+          width: 2,
         }),
-        new Style({
-          stroke: new Stroke({
-            color: `rgba(255, 0, 0, 1)`,
-            width: 2,
-          }),
-          fill: new Fill({
-            color: infiltrationColor,
-          }),
+        fill: new Fill({
+          color: color,
         }),
-        new Style({
-          stroke: new Stroke({
-            color: `rgba(255, 0, 0, 1)`,
-            width: 2,
-          }),
-          fill: new Fill({
-            color: evaporationColor,
-          }),
-        }),
-      ];
-    },
-
-    /**
-     * Adds calculated features to the abimo calculated layer.
-     * @param {Array<Feature>} calculatedFeatures - The features to be added to the layer.
-     * @return {void}
-     */
-    addToLayer(calculatedFeatures) {
-      this.layer_abimo_calculated.values_.source.addFeatures(
-        calculatedFeatures,
-      );
-    },
-    selectedStyle(styleId) {
-      console.log("[AbimoCalcButton] styleId::", styleId);
+      });
     },
   },
 };
@@ -223,33 +212,7 @@ export default {
     <div
       v-if="isCalculated"
       class="layer-abimo-calculated"
-    >
-      <label
-        for="styleSelect"
-        class="style-label"
-      >
-        {{ "choose layer" }}
-      </label>
-      <select
-        id="styleSelect"
-        class="form-select form-select-sm"
-        @change="updateStyle($event.target.value)"
-      >
-        <option
-          v-for="style in [
-            { id: 'surface_run_off' },
-            { id: 'infiltration' },
-            { id: 'evaporation' },
-          ]"
-          :key="style.id"
-          class="float-start"
-          :value="style.id"
-          :selected="selectedStyle(style.id)"
-        >
-          {{ style.id }}
-        </option>
-      </select>
-    </div>
+    ></div>
 
     <button
       class="btn btn-primary"
