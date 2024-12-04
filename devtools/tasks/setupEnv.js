29a3bb654beb03e7c3c51eaa46a7ebeda337e720
Luise Brandenburger <luise.brandenburger@ts.berlin>
2024-12-04 11:41:07 +0100

    fix: setup env script

--- devtools/tasks/setupEnv.js
+++ devtools/tasks/setupEnv.js
@@ -1,11 +1,17 @@
 const fs = require('fs');
 const path = require('path');
 
-const envExample = `API_URL=https://api.example.com`;
+const envExample = `API_URL=https://rapimo.api-base.de
+API_URL_COLOGNE=https://rapimo.api-base-cologne.de
+API_URL_BERLIN=https://rapimo.api-base-berlin.de`;
 
-const envLocal = `API_URL=https://0.0.0.0:443`;
+const envLocal = `API_URL=https://0.0.0.0:443
+API_URL_COLOGNE=https://0.0.0.0:443
+API_URL_BERLIN=https://0.0.0.0:443`;
 
-const env = `API_URL=https://api.production.com`;
+const env = `API_URL=https://api.production.com
+API_URL_COLOGNE=https://api.production-cologne.com
+API_URL_BERLIN=https://api.production-berlin.com`;
 
 const files = [
   { name: '.env.example', content: envExample },
