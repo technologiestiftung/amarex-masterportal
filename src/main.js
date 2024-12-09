ba0386b2771082ca147f6a24201160787e65e734
Jakob Sawal <jakob.sawal@ts.berlin>
2024-12-09 10:28:17 +0100

    feat: implement workaround for localStorage clear

--- src/main.js
+++ src/main.js
@@ -13,6 +13,11 @@ import utilsLogin from "../src/modules/login/js/utilsLogin";
 // import {instantiateVuetify} from "./plugins/vuetify";
 import {initiateVueI18Next, initLanguage} from "./plugins/i18next";
 
+window.addEventListener('load', () => {
+    localStorage.clear();
+    console.log('localStorage cleared on page load');
+});
+
 let app;
 const configPath = window.location.pathname.substring(0, window.location.pathname.lastIndexOf("/") + 1) + "config.js",
     loadConfigJs = new Promise((resolve, reject) => {
