9ea43f08a1d46c554d546d37d6aeb3bef9e1563a
Luise Brandenburger <luise.brandenburger@ts.berlin>
2025-02-03 13:58:27 +0100

    feat: remove unused deltaW APIs

--- addons/abimoHandler/api/getRabimo.js
+++ addons/abimoHandler/api/getRabimo.js
@@ -22,50 +22,6 @@ async function getMultiblock(payload) {
     throw error;
   }
 }
-/**
- * Tests Rabimo API
- * @param {*} endpoint
- */
-async function getTest() {
-  try {
-    const response = await fetch(`${process.env.API_URL}/calculate_all`, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.error("Error fetching data from Rabimo API:", error);
-    throw error;
-  }
-}
 
-/**
- * Fetches data from Rabimo API asynchronously
- * @param {Object} payload - data to send
- * @returns {Promise} - a promise that resolves to the response data
- */
-async function getMultiblockDeltaW(payload) {
-  try {
-    const response = await fetch(
-      `${process.env.API_URL}/calculate_multiblock_delta_w`,
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(payload),
-      },
-    );
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.error("Error fetching data from Rabimo API:", error);
-    throw error;
-  }
-}
-
-export default { getMultiblock, getTest, getMultiblockDeltaW };
+export default { getMultiblock };
 
