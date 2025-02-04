1b727f9ce0985edb3d99e1ed19a5604bc2015096
Luise Brandenburger <luise.brandenburger@ts.berlin>
2025-02-04 11:49:36 +0100

    feat: reset target input

--- addons/abimoHandler/components/AbimoHandler.vue
+++ addons/abimoHandler/components/AbimoHandler.vue
@@ -272,6 +272,9 @@ export default {
           .values_.source.clear();
 
         await this.setPreselectedFeatures(this.selectedFeatures);
+        this.setNewGreenRoof(0);
+        this.setNewUnpvd(0);
+        this.setNewToSwale(0);
         await this.setSelectedFeatures([]);
         await this.updateAccumulatedStats();
         this.setBlockAreaConfirmed(false);
