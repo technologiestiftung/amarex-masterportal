1be8c4776e1134ad4d8787492215470b84318b1b
Luise Brandenburger <luise.brandenburger@ts.berlin>
2025-04-07 12:53:38 +0200

    feat: add delta w layer

--- addons/abimoHandler/components/AbimoCalcButton.vue
+++ addons/abimoHandler/components/AbimoCalcButton.vue
@@ -57,6 +57,12 @@ export default {
       .getLayers()
       .getArray()
       .find((layer) => layer.get("id") === "abimo_result_delta_w");
+
+    this.layer_delta_w = mapCollection
+      .getMap("2D")
+      .getLayers()
+      .getArray()
+      .find((layer) => layer.get("id") === "delta_w_wfs");
   },
   methods: {
     ...mapActions("Modules/AbimoHandler", ["updateResultStats"]),
