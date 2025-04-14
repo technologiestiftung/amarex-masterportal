377bc6b59e5d671de9825b93dd701fdd8f83a57d
Luise Brandenburger <luise.brandenburger@ts.berlin>
2025-04-14 12:34:58 +0200

    feat: update constants import

--- addons/abimoHandler/utils/measureCalculations.js
+++ addons/abimoHandler/utils/measureCalculations.js
@@ -1,11 +1,14 @@
-import { MEASURE_DIMENSIONS } from "./constants.js";
+import Constants from "./constants.js"; // Importiere das gesamte Objekt
 
 function getMeasureDimension(type, size) {
-  if (!MEASURE_DIMENSIONS[type] || !MEASURE_DIMENSIONS[type][size]) {
+  if (
+    !Constants.MEASURE_DIMENSIONS[type] ||
+    !Constants.MEASURE_DIMENSIONS[type][size]
+  ) {
     console.warn(`Invalid measure type or size: ${type}, ${size}`);
     return null;
   }
-  return MEASURE_DIMENSIONS[type][size];
+  return Constants.MEASURE_DIMENSIONS[type][size];
 }
 
 export default {
