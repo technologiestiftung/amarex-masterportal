1b9cbf21fba6de7006686c1f4b5055767acba44a
Luise Brandenburger <luise.brandenburger@ts.berlin>
2025-04-28 12:05:25 +0200

    feat: add toolDownloader Addon

---
+++ addons/toolDownloader/components/ToolDownloader.vue
@@ -0,0 +1,44 @@
+<script>
+import { mapGetters } from "vuex";
+
+/**
+ * ToolDownloader
+ * @module modules/ToolDownloader
+ */
+export default {
+  name: "ToolDownloader",
+  components: {},
+  data() {
+    return {};
+  },
+  computed: {
+    ...mapGetters("Menu", ["currentComponent", "currentComponentName"]),
+  },
+  created() {
+    console.log(
+      "[ToolDownloader] toolDownloader currentComponent::",
+      this.currentComponent("secondaryMenu"),
+      this.currentComponentName("secondaryMenu"),
+    );
+  },
+  methods: {},
+};
+</script>
+
+<template lang="html">
+  <div id="tool-downloader">
+    <h2 class="mb-5 mt-3">Tool Downloader</h2>
+
+    <a>
+      <button>Bestehendes Projekt Öffnen</button>
+    </a>
+    <a>
+      <button>Bestehendes Projekt Öffnen</button>
+    </a>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@import "~variables";
+</style>
+
---
+++ addons/toolDownloader/store/stateToolDownloader.js
@@ -0,0 +1,9 @@
+/**
+ * User type definition
+ * @typedef {Object} ToolDownloaderState
+ */
+
+export default {
+  description: "Download von Dateien",
+  icon: "bi-box-arrow-in-up",
+};
