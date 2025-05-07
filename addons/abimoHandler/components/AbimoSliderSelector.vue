6bb6048f4b927df5c1ece7b77ee4ed181b6e9146
Luise Brandenburger <luise.brandenburger@ts.berlin>
2025-05-07 15:41:11 +0200

    fix: remove defaults

--- addons/abimoHandler/components/AbimoSliderSelector.vue
+++ addons/abimoHandler/components/AbimoSliderSelector.vue
@@ -23,8 +23,8 @@ export default {
       content: {
         greenRoof: {
           title: "Dachbegrünung",
-          description: (area = "XXXX", percentage = "XX") => {
-            // @Luise: Please input the correct area and percentage for greenRoof
+          description: (area, percentage) => {
+            // @Luise: Please input the correct area and percentage for greenRoof -> @jakob they are already included
             if (!area || !percentage)
               return "Welchen Anteil möchten Sie begrünen?";
             return `Bei den von Ihnen gewählten Flächen stehen ${area} m² (${percentage}%) Dachflächen zur Verfügung.<br><br>Welchen Anteil möchten Sie begrünen?`;
@@ -33,8 +33,8 @@ export default {
         unsealed: {
           title: "Unversiegelte Flächen",
           subTitle: "unversiegelte Fläche",
-          description: (area = "XXXX", percentage = "XX") => {
-            // @Luise: Please input the correct area and percentage for unsealed
+          description: (area, percentage) => {
+            // @Luise: Please input the correct area and percentage for unsealed -> @jakob they are already included
             if (!area || !percentage)
               return "Möchten Sie den Anteil unversiegelter Flächen variieren?";
             return `Bei den von Ihnen gewählten Flächen stehen ${area} m² (${percentage}%) unbebaute Fläche zur Verfügung.<br><br>Möchten Sie diesen Anteil variieren?`;
@@ -42,8 +42,8 @@ export default {
         },
         swaleConnected: {
           title: "Mulde",
-          description: (area = "XXXX", percentage = "XX") => {
-            // @Luise: Please input the correct area and percentage for swaleConnected
+          description: (area, percentage) => {
+            // @Luise: Please input the correct area and percentage for swaleConnected -> @jakob they are already included
             if (!area || !percentage)
               return "Welchen Anteil möchten Sie an Mulden anschließen?";
             return `Bei den von Ihnen gewählten Flächen stehen ${area} m² (${percentage}%) unbebaut versiegelte Fläche zur Verfügung.`;
