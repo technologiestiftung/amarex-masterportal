ae527ddc621ee8667abf078a54c32c44e81981ee
Luise Brandenburger <luise.brandenburger@ts.berlin>
2025-05-12 13:54:43 +0200

    fix: content

--- addons/abimoHandler/components/AbimoSliderSelector.vue
+++ addons/abimoHandler/components/AbimoSliderSelector.vue
@@ -37,7 +37,7 @@ export default {
             // @Luise: Please input the correct area and percentage for unsealed
             if (!area || !percentage)
               return "Möchten Sie den Anteil unversiegelter Flächen variieren?";
-            return `Bei den von Ihnen gewählten Flächen stehen ${area} m² (${percentage}%) unbebaute Fläche zur Verfügung.<br><br>Möchten Sie diesen Anteil variieren?`;
+            return `Bei den von Ihnen gewählten Flächen stehen ${area} m² (${percentage}%) unbebaute Fläche zur Verfügung.<br><br>Möchten Sie den Anteil unversiegelter Flächen variieren?`;
           },
         },
         swaleConnected: {
