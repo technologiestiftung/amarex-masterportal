131819c9624c6848820795d5a4c82cce9f009d7b
Luise Brandenburger <luise.brandenburger@ts.berlin>
2025-06-03 11:14:31 +0200

    fix: pre fill the report title

--- src/modules/print/components/PrintMap.vue
+++ src/modules/print/components/PrintMap.vue
@@ -215,7 +215,7 @@ export default {
     },
     created () {
         this.setServiceId(this.printServiceId);
-        this.report.title = this.projectTitle;
+        this.report.title = this.projectTitle || "Amarex Report";
         this.report.description = this.projectDescription;
     },
     mounted () {
@@ -544,16 +544,6 @@ export default {
                 this.warning = "Bitte führen Sie eine Wasserhaushaltsberechnung durch, um einen Report zu erstellen.";
                 return;
             }
-            /* console.log("triggerGenerate", {
-                areaTypesData: this.areaTypesData,
-                accumulatedAbimoStats: this.accumulatedAbimoStats,
-                selectedFeatures: this.selectedFeatures,
-                resultAbimoStats: this.resultAbimoStats,
-                preComputedStats: this.preComputedStats,
-                isMeasurePlanning: this.isMeasurePlanning,
-                accumulatedMeasureStats: this.accumulatedMeasureStats,
-                selectedMeasures: this.selectedMeasures
-            }); */
             this.reportLoading = true;
             this.print()
         },
